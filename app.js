a7cf588c3cb100e1c8f0ea19e2db58db6c799396
inspecthers <kg.geku@gmail.com>
2023-07-10 12:35:22 +0100

    fix

--- app.js
+++ app.js
@@ -1,52 +1,53 @@
-const express = require('express')
-  , path = require('path')
-  , createError = require('http-errors')
-  , cookieParser = require('cookie-parser')
-  , logger = require('morgan')
-  , { Check_API } = require('./configs/App.config')
-
-  , app = express();
+const express = require("express"),
+  path = require("path"),
+  createError = require("http-errors"),
+  cookieParser = require("cookie-parser"),
+  logger = require("morgan"),
+  cors = require("cors"),
+  { Check_API } = require("./configs/App.config"),
+  app = express();
 
 // view engine setup
 app
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
 
-  .use(logger('dev'))
+  .use(cors())
+  .use(logger("dev"))
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
   .use(cookieParser())
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, "public")))
 
   // Routes API
-  .use('/', require('./routes/index'))
+  .use("/", require("./routes/index"))
 
   /* Configuration */
-  .use('/api/v1', require('./routes/Configuration/Auth/User'))
+  .use("/api/v1", require("./routes/Configuration/Auth/User"))
 
   /*Gestion Stok matière prémière */
-  .use('/api/v1', require('./routes/Gestion Stock/Raw materials/Product'))
-  .use('/api/v1', require('./routes/Gestion Stock/Raw materials/EntreeStock'))
+  .use("/api/v1", require("./routes/Gestion Stock/Raw materials/Product"))
+  .use("/api/v1", require("./routes/Gestion Stock/Raw materials/EntreeStock"))
   // .use('/api/v1', require('./routes/Gestion Stock/Raw materials/SortieStock'))
 
   // catch 404 and forward to error handler
   .use(function (req, res, next) {
-    next(createError(404))
+    next(createError(404));
   })
 
   // error handler
   .use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error");
   });
 
 (async () => {
-  await Check_API()
-})()
+  await Check_API();
+})();
 
 module.exports = app;
